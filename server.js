a0fb40e590433a472688ff8a9c5a5f7a8d77a561
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-12 19:03:18 -0700

    Add babel to project

---
+++ server.js
@@ -0,0 +1,20 @@
+import bodyParser from 'body-parser';
+let PORT = 3000;
+
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config');
+var app = new require('express')();
+var compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+app.use(bodyParser.json());
+
+app.listen(PORT, error => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", PORT, PORT);
+  }
+});
