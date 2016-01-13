520703315f22a91d3d3b9549d6c278fa17347e94
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-12 21:23:42 -0700

    Add react-router sample

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+class App extends React.Component {
+  render() {
+    return <p>{this.props.children}</p>;
+  }
+}
+
+class One extends React.Component {
+  render() {
+    return <p>One</p>;
+  }
+}
+
+class Two extends React.Component {
+  render() {
+    return <p>Two</p>;
+  }
+}
+
+let router = (
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			<Route path="step1" component={One} />
+			<Route path="step2" component={Two} />
+		</Route>
+	</Router>
+)
 
 ReactDOM.render(
-  <h1>Hello, jason!</h1>,
+  router,
   document.getElementById('app')
 );
