70b6b9649cd92bbba7c1c260bd215a4f85e9822c
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-13 20:58:08 -0700

    Remove unused imports

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, combineReducers, compose} from 'redux';
 import {Provider} from 'react-redux';
-import {Router, Route} from 'react-router';
+import {Router } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import {syncReduxAndRouter, routeReducer} from 'redux-simple-router';
 import getRoutes from './routes.js';
