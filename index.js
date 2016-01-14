6517606f5dffca73f622034832ec999dc0ba2e2a
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-13 20:53:02 -0700

    Use redux-simple-router

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import {createStore, combineReducers, compose} from 'redux';
+import {Provider} from 'react-redux';
+import {Router, Route} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import {syncReduxAndRouter, routeReducer} from 'redux-simple-router';
 
-import App from './containers/app';
+import reducers from './reducers';
+
+// // Redux DevTools store enhancers
+// import {devTools, persistState} from 'redux-devtools';
+// React components for Redux DevTools
+// import {DevTools, DebugPanel, LogMonitor} from 'redux-devtools/lib/react';
+
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routeReducer
+}));
+
+// const finalCreateStore = __DEVTOOLS ? compose(
+//   // Provides support for DevTools:
+//   devTools(),
+//   // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+//   persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+// )(createStore) : createStore;
+
+const finalCreateStore = createStore;
+
+const store = finalCreateStore(reducer);
+const history = createBrowserHistory();
+
+syncReduxAndRouter(history, store);
+
+import App from './containers/App';
 import One from './components/One';
 import Two from './components/Two';
 
-let router = (
-	<Router history={browserHistory}>
-		<Route path="/" component={App}>
-			<Route path="step1" component={One} />
-			<Route path="step2" component={Two} />
-		</Route>
-	</Router>
+let routes = (
+	<Route path="/" component={App}>
+		<Route path="step1" component={One} />
+		<Route path="step2" component={Two} />
+	</Route>
 )
 
 ReactDOM.render(
-  router,
+  <div>
+    <Provider store={store}>
+      <Router history={history}>
+        {routes}
+      </Router>
+    </Provider>
+  </div>,
   document.getElementById('app')
 );
