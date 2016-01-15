19c349b796d8886ad34aa40acc7a594393b4247f
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-15 07:17:04 -0700

    Add react-router to app

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import CounterApp from './CounterApp';
 import DevTools from './DevTools';
+import { Router, Route } from 'react-router';
+
+import { createHistory } from 'history';
+
+const history = createHistory();
+
+import App from './app';
+// import Foo from './../components/foo'
+// import Bar from './../components/bar'
 
 export default class Root extends Component {
   render() {
     const { store } = this.props;
     return (
       <Provider store={store}>
-        <div>
-          <CounterApp />
-          <DevTools />
-        </div>
+        <Router history={history}>
+          <Route path="/" component={CounterApp}>
+          </Route>
+        </Router>
       </Provider>
     );
   }
