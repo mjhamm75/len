4c4dce367129ceb88a0389a6e45e89fa6f9428eb
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-16 11:38:36 -0700

    Move history to index.js

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Router, Route } from 'react-router';
-import { createHistory } from 'history';
 import App from './App';
 import PersonalInfo from './../components/PersonalInfo';
 
-const history = createHistory();
-
 export default class Root extends Component {
   render() {
+  	let history = this.props.history;
     return (
       <Router history={history}>
         <Route path="/" component={App}>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
+import { createHistory } from 'history';
 import configureStore from './store/configureStore';
 import Root from './containers/Root';
 
+const history = createHistory();
 const store = configureStore();
 
 render(
-	<Provider store={store}>
-		<Root />
+	<Provider store={store} >
+		<Root history={history} />
 	</Provider>,
   document.getElementById('root')
 );
