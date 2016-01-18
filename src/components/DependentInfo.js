a79cfb203439d427d71a09c9f1a507460d4af9a0
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-17 23:01:45 -0700

    Add dependents dynamically

--- src/components/DependentInfo.js
+++ src/components/DependentInfo.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import DependentFormField from './DependentFormField';
 
 export default class DependentInfo extends Component {
+	constructor() {
+		super();
+		this.state = {
+			dependents: []
+		}
+		this.addDependent = this.addDependent.bind(this);
+	}
+
+	addDependent() {
+		this.state.dependents.push(1);
+		this.setState({
+			dependents: this.state.dependents
+		})
+	}
+
 	renderDependents(count) {
 		return count.map((dependent, i) => {
 			return <DependentFormField key={i} index={i}/>
@@ -9,11 +24,13 @@ export default class DependentInfo extends Component {
 	}
 
 	render() {
-		let dependentsDOM = this.renderDependents([1, 2]);
+		let dependentsDOM = this.renderDependents(this.state.dependents);
 		return (
 			<div>
-				<h2>Dependent Info</h2>
+				<h2>Dependent Info</h2>				
+				<button onClick={this.addDependent}>Add Dependent</button>
 				{dependentsDOM}
+				<br />
 				<button onClick={this.saveAndContinue}>Save and Continue</button>
 			</div>
 		)
