c2b99f5fa487d529ad6662cb3065f3581cc363bb
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-18 15:32:39 -0700

    Create default state object

--- src/reducers/personal.info.js
+++ src/reducers/personal.info.js
@@ -1,4 +1,19 @@
-export default function personalInfo(state = {}, action) {
+let defaultState = {
+	name: null,
+	ssn: null,
+	dob: null,
+	phones: [],
+	email: null,
+	address: {
+		street1: null,
+		street2: null,
+		city: null,
+		state: null,
+		zipcode: null
+	}
+}
+
+export default function personalInfo(state = defaultState, action) {
 	switch(action.type) {
 		case "UPDATE_PERSONAL_INFO":
 			state[action.key] = action.value;
