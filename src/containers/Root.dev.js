4611c0f38b7e339709676ba1c0eb1948eadfda99
Jason Hamm <jasonhamm.me@gmail.com>
2016-01-19 06:10:09 -0700

    More routes to const to stop react-router warning

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -5,16 +5,19 @@ import PersonalInfo from './../components/PersonalInfo';
 import SpouseInfo from './../components/SpouseInfo';
 import DependentInfo from './../components/DependentInfo';
 
+const routes = (
+  <Route path="/" component={App}>
+    <Route path="step1" component={PersonalInfo} />
+    <Route path="step2" component={SpouseInfo} />
+    <Route path="step3" component={DependentInfo} />
+  </Route>
+)
 export default class Root extends Component {
   render() {
-  	let history = this.props.history;
+    let history = this.props.history;
     return (
       <Router history={history}>
-        <Route path="/" component={App}>
-          <Route path="step1" component={PersonalInfo} />
-          <Route path="step2" component={SpouseInfo} />
-          <Route path="step3" component={DependentInfo} />
-        </Route>
+        {routes}
       </Router>
     );
   }
