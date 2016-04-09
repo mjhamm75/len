9b8f63b0cbb14476ab700ab0982442a17751d8b6
mjhamm75 <jason.hamm@ldschurch.org>
2016-04-08 22:15:30 -0600

    Remove unused imports

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import { browserHistory } from 'react-router'
-import { syncHistory, routeReducer } from 'redux-simple-router'
+import { syncHistory } from 'redux-simple-router'
 
 const reduxRouterMiddleware = syncHistory(browserHistory);
 
