9fd31402361d7c5e291c47491e826ad4fc9b80b2
mjhamm75 <jason.hamm@ldschurch.org>
2016-04-11 23:05:54 -0600

    Update multiselect to add multiple rows

--- src/components/MultiAdd.js
+++ src/components/MultiAdd.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 
 class MultiAdd extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rows: []
+    }
+  }
+
   render() {
+    let rows = this.renderRowsDom();
     return (
       <div>
-        <a className="button small">{this.props.label}</a>
+        <a className="button small" onClick={this.add.bind(this)}>{this.props.label}</a>
+         {rows}
       </div>
     )
+  }
+
+  renderRowsDom() {
+    return this.state.rows.map(row => {
+      return <input />
+    });
+  }
+
+  add() {
+    let rows = this.state.rows;
+    rows.push({
+      name: undefined,
+      value: undefined
+    })
+    this.setState({
+      rows: rows
+    });
   }
 }
 
