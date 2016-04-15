b10923292f95e531d1035b58474b00a64ff6ddd6
mjhamm75 <jason.hamm@ldschurch.org>
2016-04-14 22:53:34 -0600

    Reimplement SpouseInfo

--- src/components/Address.js
+++ src/components/Address.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 
 class Address extends Component {
   render() {
+    let showSameAddress = !this.props.showSame ? {
+      display: 'none'
+    } : null;
     return (
       <div>
+        <div style={showSameAddress} className="checkbox">
+          <input
+            name="sameAddress"
+            onChange={this.props.onCheck}
+            type="checkbox"
+            />
+          <label>Same as Spouse</label>
+        </div>
         <div className="field">
           <label>Street 1</label>
           <input />
--- src/components/SpouseInfo.js
+++ src/components/SpouseInfo.js
@@ -1,109 +1,49 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { updateSpouseInfo, syncAddress } from './../actions/Actions';
-import FormField from './FormField';
 import { routeActions } from 'redux-simple-router'
-var fields = require('./mock.fields.js');
+import { connect } from 'react-redux';
+
+import Contact from './Contact';
+import Address from './Address';
+import Married from './Married';
+import { syncAddress, updateSpouseInfo } from './../actions/Actions'
 
 class SpouseInfo extends Component {
-  constructor() {
-    super();
-    this.updateValue = this.updateValue.bind(this);
-    this.saveAndContinue = this.saveAndContinue.bind(this);
-    this.updateSame = this.updateSame.bind(this);
-    this.mergeAddress = this.mergeAddress.bind(this);
-    this.createAddress = this.createAddress.bind(this);
-    this.state = {
-      same: false
-    }
-  }
-
-  updateValue(value) {
-    let key = value.target.name;
-    let val = value.target.value;
-    this.props.dispatch(updateSpouseInfo(key, val));
-  }
-
-  saveAndContinue() {
-    this.props.dispatch(routeActions.push('/dependents'));
-  }
-
-  renderFields() {
-    return fields.name.map((field, i) => {
-      return field.title ? <FormField key={i} title={field.title} name={field.name} type={field.type} updateValue={this.updateValue} /> :
-                  <h4 key={i} >{field.header}</h4>
-    });
-  }
-
-  renderAddress(same) {
-    return fields.spouseAddress.map((field, i) => {
-      let value = same ? this.props.spouseInfo[field.name] : null;
-      return <FormField
-                disabled={same}
-                key={i}
-                name={field.name}
-                title={field.title}
-                value={value}
-              />
-    });
-  }
-
   render() {
-    let same = this.state.same;
-    let fieldsDOM = this.renderFields();
-    let addressDOM = this.renderAddress(same);
     return (
       <div>
-        <h2>Spouse Info</h2>
-        <form className="pure-form pure-form-stacked">
-          {fieldsDOM}
-        </form>
-        <h3>Address</h3>
-        <input ref="same" type="checkbox" onChange={this.updateSame}></input> Same as spouse
-        <br />
-        <form className="pure-form pure-form-stacked">
-          {addressDOM}
-        </form>
-        <br />
-        <button className="pure-button" onClick={this.saveAndContinue}>Save and Continue</button>
+        <div className="flex">
+          <Contact
+            onChange={this.onChange.bind(this)}
+          />
+          <Address
+            onChange={this.onChange.bind(this)}
+            onCheck={this.onCheck.bind(this)}
+            showSame={true}
+          />
+        </div>
+        <a className="button pull-right" onClick={this.continue.bind(this)}>Next</a>
       </div>
     )
   }
 
-  updateSame() {
-    this.setState({
-      same: this.refs.same.checked
-    })
-    if(this.refs.same.checked) {
-      this.mergeAddress();
-    }
+  onCheck(e) {
+    let checked = e.target.checked;
+    this.props.dispatch(syncAddress(checked));
   }
 
-  mergeAddress() {
-    let address = this.createAddress();
-    this.props.dispatch(syncAddress(address));
+  onChange(obj) {
+    this.props.dispatch(updateSpouseInfo(obj));
   }
 
-  createAddress() {
-    return {
-      street1: this.props.personalInfo.street1,
-      street2: this.props.personalInfo.street2,
-      city: this.props.personalInfo.city,
-      state: this.props.personalInfo.state,
-      zipcode: this.props.personalInfo.zipcode
-    }
+  continue() {
+    this.props.dispatch(routeActions.push('/dependents'));
   }
 }
 
 function mapStateToProps(state) {
-  let spouseInfo = state.spouseInfo;
-  let personalInfo = state.personalInfo
   return {
-    spouseInfo,
-    personalInfo
+    spouseInfo: state.spouseInfo
   }
 }
 
-export default connect(
-  mapStateToProps
-)(SpouseInfo);
+export default connect(mapStateToProps)(SpouseInfo);
