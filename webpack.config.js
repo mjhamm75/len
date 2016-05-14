1b3371282be8891787e5a63d350085ca3019f324
mjhamm75 <jason.hamm@ldschurch.org>
2016-05-14 09:06:10 -0600

    Show source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool: 'eval',
+    devtool: 'source-map',
     entry: [
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
