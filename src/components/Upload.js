db31da86db6dfe6084637e5f894182abf3dc721c
mjhamm75 <jason.hamm@ldschurch.org>
2016-05-20 00:24:41 -0600

    Add file uploader component

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -4,20 +4,39 @@ import { connect } from 'react-redux';
 import Dropzone from 'react-dropzone';
 
 class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: []
+    }
+  }
+
+  renderFiles(files) {
+    return files.map((file, index) => {
+      return <div><img key={index} src={file.preview} /> )}</div>
+    });
+  }
+
 	render() {
+    let filesDOM = this.renderFiles(this.state.files);
     return (
       <div>
-        <div className="drop-container">
-          <Dropzone onDrop={this.onDrop}>
-            <div className="text">Drop file or click to add</div>
-          </Dropzone>
-        </div>
+          <div>
+            <div className="drop-container">
+              <Dropzone onDrop={this.onDrop.bind(this)}>
+                <div className="text">Drop file or click to add</div>
+              </Dropzone>
+            </div>
+          </div>
+          {filesDOM}
       </div>
     )
   }
 
   onDrop(files) {
-    console.log('Received files: ', files);
+    this.setState({
+      files: files
+    });
   }
 }
 
